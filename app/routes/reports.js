f840644514e69daf8530fff2c7b33bc41740b1b8
Matt Spell <matthew.spell1@gmail.com>
2014-06-23 10:49:08 -0500

    styling

--- app/routes/reports.js
+++ app/routes/reports.js
@@ -9,7 +9,7 @@ var request = require('request');
 exports.create = (req, res)=>{
   Report.create(req.user._id, req.body, (report)=>{
     var street = req.body.streetName.split(' ').map(each=>each.trim());
-    request('https://maps.googleapis.com/maps/api/geocode/json?address='+street[0]+'+'+street[1]+'+'+street[2]+',+Nashville,+TN&key=AIzaSyCCisv6D7SOLh8w4s58alflk1tk9qlCFQ0', function(err, response, body){
+    request('https://maps.googleapis.com/maps/api/geocode/json?address='+street[0]+'+'+street[1]+'+'+street[2]+'+'+street[3]+',+Nashville,+TN&key=AIzaSyCCisv6D7SOLh8w4s58alflk1tk9qlCFQ0', function(err, response, body){
     if(!err && response.statusCode === 200){
       body = JSON.parse(body);
       var lat = body.results[0].geometry.location.lat;
